7e8410697a4788f0ebd73a1af995911acda8930f
vlbras <vlad.bras02@gmail.com>
2024-01-26 23:22:44 +0100

    feat: integrate class-validator and class-transformer

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { MongoExceptionFilter } from './common';
+import { ValidationPipe } from '@nestjs/common/pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new MongoExceptionFilter());
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
