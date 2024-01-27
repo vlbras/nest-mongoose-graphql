31372abbcc48443eec0bb78cebc509e356c977c4
vlbras <vlad.bras02@gmail.com>
2024-01-27 18:17:31 +0100

    feat: integrate nestjs-object-id

--- src/users/dto/get-user.input.ts
+++ src/users/dto/get-user.input.ts
@@ -1,10 +1,13 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { IsEmail, IsOptional, IsUUID } from 'class-validator';
+import { IsObjectId } from 'nestjs-object-id';
 import { UserStatuses } from 'src/common';
 
 @InputType()
 export class GetUserInput {
   @Field(() => String, { nullable: true })
+  @IsOptional()
+  @IsObjectId()
   _id?: string;
 
   @Field(() => String, { nullable: true })
